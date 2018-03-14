c5b350375f8439a17c18329ebf500bc5b33bd9f3
jmills-ncar <jmills@ucar.edu>
2018-03-14 13:31:31 -0600

    reformatted to standards

--- wrfhydro/utilities.py
+++ wrfhydro/utilities.py
@@ -13,38 +13,40 @@ def compare_restarts(candidate_restart: str,
     Args:
         candidate_restart: The path for the candidate restart file
         ref_restarts: The path for the reference restart file
-        exclude_vars: A list of strings containing variables names to exclude from the comparison
+        exclude_vars: A list of strings containing variables names to
+        exclude from the comparison
     Returns:
         Either a pandas dataframe if possible or subprocess object
     """
 
-    #Convert exclude_vars list into a comman separated string
+    # Convert exclude_vars list into a comman separated string
     exclude_vars = ','.join(exclude_vars)
 
-    #Make list to pass to subprocess
-    command_list = ['nccmp', '-dmfq', '-S','-x '+exclude_vars,
+    # Make list to pass to subprocess
+    command_list = ['nccmp', '-dmfq', '-S', '-x ' + exclude_vars,
                     candidate_restart,
                     ref_restart]
 
     #Run the subprocess to call nccmp
     proc = subprocess.run(command_list,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE)
 
     #Check return code
     if proc.returncode != 0:
-        #Get stoud into stringio object
+        # Get stoud into stringio object
         output = StringIO()
         output.write(proc.stdout.decode('utf-8'))
         output.seek(0)
 
-        #Open stringio object as pandas dataframe
+        # Open stringio object as pandas dataframe
         try:
-            nccmpOut = pd.read_table(output,delim_whitespace=True,header=0)
-            return (nccmpOut)
+            nccmp_out = pd.read_table(output,delim_whitespace=True,header=0)
+            return (nccmp_out)
         except:
-            warn('Probleming reading nccmp output to pandas dataframe, returning as subprocess object')
-            return(proc)
+            warn('Probleming reading nccmp output to pandas dataframe,'
+                 'returning as subprocess object')
+            return proc
     else:
-        return('No differences found')
+        return 'No differences found'
 

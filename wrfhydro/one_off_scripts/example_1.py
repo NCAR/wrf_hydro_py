deff6bf80a21c3d059cf281c283b5511df7ced70
jmills-ncar <jmills@ucar.edu>
2018-03-19 10:45:07 -0600

    refactored run function into class to help with attribute docs

--- wrfhydro/one_off_scripts/example_1.py
+++ wrfhydro/one_off_scripts/example_1.py
@@ -1,3 +1,10 @@
+#docker pull wrfhydro/domains:croton_lite
+#docker create --name croton_lite
+#docker run --volumes-from croton_lite -it wrfhydro/dev:conda
+#git clone https://github.com/jmills-ncar/wrf_hydro_py.git
+#git clone https://github.com/jmills-ncar/wrf_hydro_nwm_public.git
+#python3
+
 import sys
 sys.path.insert(0, '/home/docker/wrf_hydro_py/wrfhydro')
 
@@ -5,8 +12,8 @@ from wrf_hydro_model import *
 from test_cases import *
 from utilities import *
 
-wrfModel = WrfHydroModel('/home/docker/wrf_hydro_nwm/trunk/NDHMS')
-wrfDomain = WrfHydroDomain(domain_top_dir='/home/docker/domain/croton_NY',
+wrfModel = WrfHydroModel('/home/docker/wrf_hydro_nwm_public/trunk/NDHMS')
+wrfDomain = WrfHydroDomain(domain_top_dir='/home/docker/domain/croton_lite',
                           domain_config='NWM',
                            model_version='v1.2.1')
 

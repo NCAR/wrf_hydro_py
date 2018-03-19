09429d8332338bca2d5e505384cc4f20ed44780d
James L McCreight <mccreigh@gmail.com>
2018-03-18 22:29:27 -0600

    Docker example1

--- wrfhydro/one_off_scripts/example_1.py
+++ wrfhydro/one_off_scripts/example_1.py
@@ -1,3 +1,15 @@
+docker create --name croton wrfhydro/domains:croton_NY
+docker run -it \
+    -v /Users/james/WRF_Hydro/wrf_hydro_nwm_myFork:/wrf_hydro_nwm \
+    -v /Users/james/WRF_Hydro/wrf_hydro_py/:/home/docker/wrf_hydro_py \
+    --volumes-from croton \
+    wrfhydro/dev:conda
+
+#######################################################
+cp -r /wrf_hydro_nwm /home/docker/wrf_hydro_nwm
+python
+
+#######################################################
 import sys
 sys.path.insert(0, '/home/docker/wrf_hydro_py/wrfhydro')
 
@@ -5,10 +17,11 @@ from wrf_hydro_model import *
 from test_cases import *
 from utilities import *
 
-wrfModel = WrfHydroModel('/home/docker/wrf_hydro_nwm/trunk/NDHMS')
-wrfDomain = WrfHydroDomain(domain_top_dir='/home/docker/domain/croton_NY',
-                          domain_config='NWM',
-                           model_version='v1.2.1')
+model = WrfHydroModel('/home/docker/wrf_hydro_nwm/trunk/NDHMS')
+
+domain = WrfHydroDomain(domain_top_dir='/home/docker/domain/croton_NY',
+                        domain_config='NWM',
+                        model_version='v1.2.1')
 
 wrfModel.compile('gfort')
 

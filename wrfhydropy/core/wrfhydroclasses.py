b441fddedc338293f4653cd0890fe56b5c3a4bf0
jmills-ncar <jmills@ucar.edu>
2018-06-27 14:46:44 -0600

    debugging comile cmd

--- wrfhydropy/core/wrfhydroclasses.py
+++ wrfhydropy/core/wrfhydroclasses.py
@@ -200,15 +200,15 @@ class WrfHydroModel(object):
         # Create compile command for machine spec
         if self.machine_spec is not None:
             modules = ' '.join(self.machine_spec['modules'][self.compiler])
-            compile_cmd = shlex.split(
-                '/bin/bash -c "' +
-                'module purge && module load ' +
-                modules +
-                ' && ' +
-                './compile_offline_NoahMP.sh ' +
-                str(compile_options_file.absolute()) +
+            compile_cmd = '/bin/bash -c "' + \
+                'module purge && module load ' + \
+                modules + \
+                ' && ' + \
+                './compile_offline_NoahMP.sh ' + \
+                str(compile_options_file.absolute()) + \
                 '"'
-            )
+            print(compile_cmd)
+            compile_cmd = shlex.split(compile_cmd)
             print(compile_cmd)
         else:
             compile_cmd = shlex.split(

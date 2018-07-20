298b3e09087d25ce1806bd0b624b2d1bc1e80a90
James McCreight <jamesmcc@ucar.edu>
2018-07-20 16:40:19 -0600

    Refactor the dart object

--- wrfhydropy/core/dartclasses.py
+++ wrfhydropy/core/dartclasses.py
@@ -17,49 +17,58 @@ from .utilities import \
 from .job import Job, Scheduler
 
 
-class DartExec(object):
+class DartWork(object):
     def __init__(
         self,
-        tag,
-        source_dir,
-        target_dir: pathlib.PosixPath=None
+        path_rel,
+        path_dart,
+        build_dir,
+        mpi
     ):
 
-        # If target_dir, the executable and namelist identified by tag in
-        # source_dir are copied and referenced in target_dir.
+        self.path_rel = path_rel
 
-        self.exe_file = None
-        """The DART executable pathlib.PosixPath"""
-        self.input_nml_file = None
-        """The input nml pathlib.PosixPath for this executable"""
-        self.input_nml = dict()
-        """The namelist dictionary for the executable."""
+        self.input_nml_file = path_dart / self.path_rel / 'input.nml'
+        self.input_nml = f90nml.read(self.input_nml_file)
 
-        input_nml_file_source = source_dir / ('input.nml.' + tag + '_default')
-        if input_nml_file_source.exists():
-            if target_dir is not None:
-                input_nml_file_target = target_dir / ('input.nml.' + tag + '_default')
-                shutil.copy(str(input_nml_file_source), str(input_nml_file_target))
-            else:
-                input_nml_file_target = input_nml_file_source
+        if build_dir != path_dart:
+            self.input_nml_file = build_dir / self.path_rel / 'input.nml'
+            (build_dir / self.path_rel).mkdir(exist_ok=True, parents=True)
+            self.input_nml.write(self.input_nml_file)
 
-            self.input_nml_file = input_nml_file_target
-            self.input_nml = f90nml.read(self.input_nml_file)            
-        else:
-            self.input_nml_file = None
-            self.input_nml = None
+        self.compile(path_dart, path_rel, mpi)
 
-        exe_file_source = source_dir / tag
-        if exe_file_source.exists():
-            if target_dir is not None:
-                exe_file_target = target_dir / tag
-                shutil.copy(str(exe_file_source), str(exe_file_target))
-            else:
-                exe_file_target = exe_file_source
+        # list the mkmfs and get the binaries.
+        mkmfs = list((pathlib.PosixPath(path_dart) / path_rel).glob('mkmf_*'))
+        dart_exes = [pathlib.PosixPath(str(mm).replace('/mkmf_','/')) for mm in mkmfs]
+        build_exes = [(build_dir / path_rel / dd.name) for dd in dart_exes]
+        _ = [shutil.copy(str(dd), str(ss)) for dd, ss in zip(dart_exes, build_exes)] 
+        self.exes = {bb.name: bb for bb in build_exes}
 
-            self.exe_file = exe_file_target
-        else:
-            self.exe_file = None
+
+    def compile(
+        self,
+        path_dart,
+        path_rel,
+        mpi
+    ):
+
+        # compile each workdir
+        build_cmd = './quickbuild.csh'
+        if mpi:
+            build_cmd += ' -mpi'
+
+        print(path_rel + ': Running "' + build_cmd + '"')
+        self.compile_subproc = subprocess.run(
+            shlex.split(build_cmd),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=path_dart / path_rel
+        )
+
+        if self.compile_subproc.returncode != 0:
+            raise ValueError('DART did not successfully compile.')
+
 
 
 class DartSetup(object):
@@ -68,170 +77,57 @@ class DartSetup(object):
         self,
         source_dir: str,
         mkmf_template: str,
-        input_nml_file: str='models/wrfHydro/work/input.nml',
-        model_work_dir: str='models/wrfHydro/work',
+        work_dirs: list=['models/wrfHydro/work'],
         mpi: bool=True, 
         build_dir: str = None,
         overwrite: bool = False
     ):
-        """Instantiate a WrfHydroModel object.
-        Args:
-            source_dir: Directory containing the source code, e.g.
-               'wrf_hydro_nwm/trunk/NDHMS'.
-            mkmf_template: the file to use fo mkmf.
-            build_dir: Optional, new directory to to hold results
-               of code compilation.
-        Returns:
-            A DartSetup object.
-        """
-        # Instantiate all attributes and methods
-        self.source_dir = source_dir
-        """pathlib.Path: pathlib.Path object for source code directory."""
-        self.mkmf_template = mkmf_template
-        self.model_work_dir = model_work_dir
-        self.mpi = mpi       
-        self.build_dir = build_dir
 
-        self.input_nml_file = input_nml_file
-        self.input_nml = dict()
-        """dict: the input_nml stored with the source code."""
-        
-        self.compiler = None
-        #self.modules = None
-        self.git_hash = None
-        self.compile_log = None
-        """CompletedProcess: The subprocess object generated at compile."""
-        self.object_id = None
-        """str: A unique id to join object to compile directory."""
-
-        self.preprocess = None
-        self.create_fixed_network_seq = None
-        self.create_obs_sequence = None
-        self.model_mod_check = None
-        self.obs_diag = None
-        self.obs_seq_to_netcdf = None
-        self.obs_sequence_tool = None
-        self.perfect_model_obs = None
-        self.filter = None
-        """DartExec classes for compile-time executables and their namelists.."""
-        # TODO(JLM): move these to a list called "executables"?
-
-        ## Setup directory paths
         self.source_dir = pathlib.PosixPath(source_dir).absolute()
-        self.mkmf_template = self.source_dir / ('build_templates/' + self.mkmf_template)
-        self.model_work_dir = self.source_dir / self.model_work_dir
-        if self.build_dir is None:
-            self.build_dir = self.model_work_dir
-        else:
-            self.build_dir = pathlib.PosixPath(self.build_dir)
-            self.build_dir.mkdir()
-            # TODO(JLM): enforce that basename(build_dir) is experiment_dir
+        self.mkmf_template = self.source_dir / ('build_templates/' + mkmf_template)
+        if type(work_dirs) is not list:
+            work_dirs = [work_dirs]
+        self.work_dirs = [pathlib.PosixPath(ww) for ww in work_dirs]
+        self.mpi = mpi
+        self.build_dir = build_dir
+        self.overwrite = overwrite
 
-        self.run_scripts = []
-        scripts_path = self.source_dir / 'models/wrfHydro/shell_scripts/'
-        # Build this with the list of scripts to include in the run dir
-        # Should remain static... or it could be an option. seems unlikely.
-        scripts_basenames = ['advance_ensemble.py']
-        self.run_scripts = [scripts_path / nn for nn in scripts_basenames]
-
-        ## Load master namelists
-        # TODO(JLM): allow flexibility in the input_nml_file, check its
-        # basename: if none, then do the following, else leave it alone.
-        self.input_nml = f90nml.read(self.source_dir / input_nml_file)
-        if self.build_dir != self.model_work_dir:
-            self.input_nml_file = self.build_dir / os.path.basename(input_nml_file)
-            self.input_nml.write(self.input_nml_file)
-
-        self.compile()
-
-    # Could probably eliminate this as a method...     
-    def compile(
-        self
-    ) -> str:
-
-        # Ignore this stuff for the moment.
-        # # A bunch of ugly logic to check compile directory.
-        # if self.compile_dir is None:
-        #     self.compile_dir = self.source_dir.joinpath('models/wrfHydro/work')
-        # else:
-        #     self.compile_dir = pathlib.Path(compile_dir).absolute()
-        #     if self.compile_dir.is_dir() is False:
-        #         self.compile_dir.mkdir(parents=True)
-        #     else:
-        #         if self.compile_dir.is_dir() is True and overwrite is True:
-        #             shutil.rmtree(str(self.compile_dir))
-        #             self.compile_dir.mkdir()
-        #         else:
-        #             raise IOError(str(self.compile_dir) + ' directory already exists')
-
-        # Add compiler and compile options as attributes and update if needed
-        #self.compiler = compiler
-        #self.modules
         self.git_hash = get_git_revision_hash(self.source_dir)
 
-        # mkmf
+        # mkmf establishment
         mkmf_dir = self.source_dir / 'build_templates'
         mkmf_target = mkmf_dir / 'mkmf.template'
         mkmf_target.unlink()
         mkmf_target.symlink_to(self.mkmf_template)
-        # TODO(JLM): Apparently, one does not need to run mkmf.
-        # I'm going to leave this here for a while till i'm sure (5/18/18)
-        #mkmf_cmd = './mkmf'
-        #print('DartSetup: Running "' + mkmf_cmd + '"')
-        #self.mkmf_log = subprocess.run(shlex.split(mkmf_cmd),
-        #                               stdout=subprocess.PIPE,
-        #                               stderr=subprocess.PIPE,
-        #                               cwd=self.source_dir / 'build_templates')
 
-        # compile
-        build_cmd = './quickbuild.csh'
-        if self.mpi:
-            build_cmd += ' -mpi'
-        print('DartSetup: Running "' + build_cmd + '"')
-        self.compile_log = subprocess.run(shlex.split(build_cmd),
-                                          stdout=subprocess.PIPE,
-                                          stderr=subprocess.PIPE,
-                                          cwd=self.model_work_dir)
+        self.object_id = None
+        """str: A unique id to join object to compile directory."""
+
+        ## Setup directory paths
+        if self.build_dir is not None:
+            self.build_dir = pathlib.PosixPath(self.build_dir)
+            self.build_dir.mkdir()
+            # TODO(JLM): enforce that basename(build_dir) is experiment_dir
+
+        for www in self.work_dirs:
+            ww = str(www)
+            dart_work = DartWork(
+                ww,
+                self.source_dir,
+                self.build_dir,
+                self.mpi
+            )
+            ww_repl = ww.replace('/','__')
+            self.__dict__.update({ww_repl:dart_work})
 
         # Add in unique ID file to match this object to prevent assosciating
         # this directory with another object
         self.object_id = str(uuid.uuid4())
-
         with open(self.build_dir.joinpath('.uid'),'w') as f:
             f.write(self.object_id)
 
-        if self.compile_log.returncode == 0:
-            # Collect the built binaries.
-
-            build_objs = [
-                'preprocess',
-                'create_fixed_network_seq',
-                'create_identity_streamflow_obs',
-                'create_obs_sequence',
-                'model_mod_check',
-                'obs_diag',
-                'obs_seq_to_netcdf',
-                'obs_sequence_tool',
-                'perfect_model_obs',
-                'filter'
-            ]
-
-            for bb in build_objs:
-                self.__dict__[bb] = DartExec(
-                    bb,
-                    self.model_work_dir,
-                    self.build_dir
-                )
-
-            # Save the object out to the compile directory
-            self.pickle()
-
-            print('DART successfully compiled into ' + str(self.build_dir))
-
-        else:
-
-            raise ValueError('DART did not successfully compile.')
-
+        self.pickle()
+        print('DART successfully compiled into ' + str(self.build_dir))
 
     def pickle(self):
         with open(self.build_dir.joinpath('DartSetup.pkl'), 'wb') as f:

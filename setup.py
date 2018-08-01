22c07434ef9a590359f1a1f7c74fa7e0540f786a
jmills-ncar <jmills@ucar.edu>
2018-08-01 16:00:19 -0600

    updated version to stable and ready to push to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wrfhydropy',
-    version='0.0.5dev4',
+    version='0.0.5',
     packages=find_packages(),
     package_data={'wrfhydropy': ['core/data/*']},
     url='https://github.com/NCAR/wrf_hydro_py',

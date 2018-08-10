923d2ab4d4da782a26bc9049328598e11e608866
jmills-ncar <jmills@ucar.edu>
2018-08-10 09:50:05 -0600

    Split output diffs into data and metadata

--- wrfhydropy/core/outputdiffs.py
+++ wrfhydropy/core/outputdiffs.py
@@ -2,14 +2,15 @@ import io
 import shlex
 import subprocess
 import warnings
+import pathlib
 
 import pandas as pd
 
 from .simulation import SimulationOutput
 
-
 def compare_ncfiles(candidate_files: list,
                     reference_files: list,
+                    stats_only: bool = False,
                     nccmp_options: list = ['--data', '--metadata', '--force'],
                     exclude_vars: list = ['ACMELT','ACSNOW','SFCRUNOFF','UDRUNOFF','ACCPRCP',
                                           'ACCECAN','ACCEDIR','ACCETRAN','qstrmvolrt',
@@ -19,6 +20,7 @@ def compare_ncfiles(candidate_files: list,
     Args:
         candidate_files: List of candidate netcdf file paths
         reference_files: List of reference netcdf file paths
+        stats_only: Only return statistics on differences in data values
         nccmp_options: List of long-form command line options passed to nccmp,
         see http://nccmp.sourceforge.net/ for options
         exclude_vars: A list of strings containing variables names to
@@ -29,13 +31,17 @@ def compare_ncfiles(candidate_files: list,
         A named list of either pandas dataframes if possible or subprocess objects
     """
 
-    ref_dir = reference_files[0].parent
+    ref_dir = pathlib.Path(reference_files[0]).parent
+
     output_list = []
     for file_candidate in candidate_files:
-        file_reference = ref_dir.joinpath(file_candidate.name)
+        file_candidate = pathlib.Path(file_candidate)
+
+        file_reference = pathlib.Path(ref_dir.joinpath(file_candidate.name))
         if file_reference.is_file():
-            nccmp_out = _compare_nc_nccmp(candidate_nc=file_candidate,
-                                          reference_nc=file_reference,
+            nccmp_out = _compare_nc_nccmp(candidate_nc=str(file_candidate),
+                                          reference_nc=str(file_reference),
+                                          stats_only=stats_only,
                                           nccmp_options=nccmp_options,
                                           exclude_vars=exclude_vars,
                                           exclude_atts=exclude_atts)
@@ -45,11 +51,78 @@ def compare_ncfiles(candidate_files: list,
     return output_list
 
 
-class OutputDiffs(object):
+class OutputDataDiffs(object):
     def __init__(self,
                  candidate_output: SimulationOutput,
                  reference_output: SimulationOutput,
                  nccmp_options: list = ['--data', '--metadata', '--force'],
+                 exclude_vars: list = ['ACMELT', 'ACSNOW', 'SFCRUNOFF', 'UDRUNOFF', 'ACCPRCP',
+                                       'ACCECAN', 'ACCEDIR', 'ACCETRAN', 'qstrmvolrt',
+                                       'reference_time'],
+                 exclude_atts: list = ['valid_min']):
+        """Calculate Diffs between SimulationOutput objects from two WrfHydroSim objects
+        Args:
+            candidate_output: The candidate SimulationOutput object
+            reference_output: The reference SimulationOutput object
+            nccmp_options: List of long-form command line options passed to nccmp,
+            see http://nccmp.sourceforge.net/ for options
+            exclude_vars: A list of strings containing variables names to
+            exclude from the comparison
+            exclude_atts: A list of strings containing attribute names to
+            exclude from the comparison
+        Returns:
+            An OutputDiffs object
+        """
+        # Instantiate all attributes
+        self.diff_counts = dict()
+        """dict: Counts of diffs by restart type"""
+
+        self.channel_rt = list()
+        """list: List of pandas dataframes if possible or subprocess objects containing nudging
+        restart file diffs"""
+        self.chanobs = list()
+        """list: List of pandas dataframes if possible or subprocess objects containing nudging
+        restart file diffs"""
+        self.lakeout = list()
+        """list: List of pandas dataframes if possible or subprocess objects containing nudging
+        restart file diffs"""
+        self.gwout = list()
+        """list: List of pandas dataframes if possible or subprocess objects containing nudging
+        restart file diffs"""
+        self.restart_hydro = list()
+        """list: List of pandas dataframes if possible or subprocess objects containing hydro
+        restart file diffs"""
+        self.restart_lsm = list()
+        """list: List of pandas dataframes if possible or subprocess objects containing lsm restart
+        file diffs"""
+        self.restart_nudging = list()
+        """list: List of pandas dataframes if possible or subprocess objects containing nudging
+        restart file diffs"""
+
+        # Create list of attributes to diff
+        atts_list = ['channel_rt','chanobs','lakeout','gwout','restart_hydro','restart_lsm',
+                     'restart_nudging']
+        for att in atts_list:
+            candidate_att = getattr(candidate_output,att)
+            reference_att = getattr(reference_output,att)
+
+            if candidate_att is not None and reference_att is not None:
+                setattr(self,att,compare_ncfiles(candidate_files=candidate_att,
+                                                 reference_files=reference_att,
+                                                 stats_only=True,
+                                                 nccmp_options=nccmp_options,
+                                                 exclude_vars=exclude_vars,
+                                                 exclude_atts=exclude_atts)
+                        )
+                diff_counts = sum(1 for _ in filter(None.__ne__, getattr(self,att)))
+                self.diff_counts.update({att: diff_counts})
+
+class OutputMetaDataDiffs(object):
+    def __init__(self,
+                 candidate_output: SimulationOutput,
+                 reference_output: SimulationOutput,
+                 stats_only=False,
+                 nccmp_options: list = ['--metadata', '--force'],
                  exclude_vars: list = ['ACMELT', 'ACSNOW', 'SFCRUNOFF', 'UDRUNOFF', 'ACCPRCP',
                                        'ACCECAN', 'ACCEDIR', 'ACCETRAN', 'qstrmvolrt',
                                        'reference_time'],
@@ -110,9 +183,9 @@ class OutputDiffs(object):
                 diff_counts = sum(1 for _ in filter(None.__ne__, getattr(self,att)))
                 self.diff_counts.update({att: diff_counts})
 
-
 def _compare_nc_nccmp(candidate_nc: str,
                       reference_nc: str,
+                      stats_only: bool = False,
                       nccmp_options: list = ['--data','--metadata','--force'],
                       exclude_vars: list = None,
                       exclude_atts: list = None):
@@ -121,7 +194,8 @@ def _compare_nc_nccmp(candidate_nc: str,
     This is wrapped by compare ncfiles to applying to a list of one or more files
     Args:
         candidate_nc: The path for the candidate restart file
-        ref_nc: The path for the reference restart file
+        reference_nc: The path for the reference restart file
+        stats_only: Only return statistics on differences in data values
         nccmp_options: List of long-form command line options passed to nccmp,
         see http://nccmp.sourceforge.net/ for options
         exclude_vars: A list of strings containing variables names to
@@ -165,16 +239,21 @@ def _compare_nc_nccmp(candidate_nc: str,
 
     #Check return code
     if proc.returncode != 0:
-        # Get stoud into stringio object
-        output = io.StringIO()
-        output.write(proc.stdout.decode('utf-8'))
-        output.seek(0)
+        if stats_only:
+            try:
+                # First try stdout because that is where statistics are written
+                # Get stoud into stringio object
+                output = io.StringIO()
+                output.write(proc.stdout.decode('utf-8'))
+                output.seek(0)
 
-        # Open stringio object as pandas dataframe
-        try:
-            nccmp_out = pd.read_table(output,delim_whitespace=True,header=0)
-            return nccmp_out
-        except:
-            warnings.warn('Problem reading nccmp output to pandas dataframe,'
-                          'returning as subprocess object')
-            return proc
+                nccmp_out = pd.read_table(output,delim_whitespace=True,header=0)
+                return nccmp_out
+            except:
+                warnings.warn('Problem reading nccmp output to pandas dataframe,'
+                              'returning as subprocess object')
+                return proc
+        else:
+            return proc.stderr.decode('utf-8') + proc.stdout.decode('utf-8')
+    else:
+        return None

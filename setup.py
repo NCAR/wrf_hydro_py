8cca05d4d22774a4a03184d6a521022bd40228c9
jmills-ncar <jmills@ucar.edu>
2018-08-10 11:16:34 -0600

    incremented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wrfhydropy',
-    version='0.0.9.dev0',
+    version='0.0.9',
     packages=find_packages(),
     package_data={'wrfhydropy': ['core/data/*']},
     url='https://github.com/NCAR/wrf_hydro_py',

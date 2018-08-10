5dc02d7b9c8fff82ac54255745c16b386d75ed5b
jmills-ncar <jmills@ucar.edu>
2018-08-10 11:16:04 -0600

    Better handling of files in OutputDiffs class

--- wrfhydropy/core/outputdiffs.py
+++ wrfhydropy/core/outputdiffs.py
@@ -12,10 +12,8 @@ def compare_ncfiles(candidate_files: list,
                     reference_files: list,
                     stats_only: bool = False,
                     nccmp_options: list = ['--data', '--metadata', '--force'],
-                    exclude_vars: list = ['ACMELT','ACSNOW','SFCRUNOFF','UDRUNOFF','ACCPRCP',
-                                          'ACCECAN','ACCEDIR','ACCETRAN','qstrmvolrt',
-                                          'reference_time'],
-                    exclude_atts: list = ['valid_min']):
+                    exclude_vars: list = None,
+                    exclude_atts: list = None):
     """Compare lists of netcdf restart files element-wise. Files must have common names
     Args:
         candidate_files: List of candidate netcdf file paths
@@ -31,23 +29,23 @@ def compare_ncfiles(candidate_files: list,
         A named list of either pandas dataframes if possible or subprocess objects
     """
 
-    ref_dir = pathlib.Path(reference_files[0]).parent
+    if len(candidate_files) != len(reference_files):
+        raise ValueError('Length of candidate files does not match len of reference files')
+
+    file_list = zip(candidate_files,reference_files)
 
     output_list = []
-    for file_candidate in candidate_files:
-        file_candidate = pathlib.Path(file_candidate)
+    for files in file_list:
+        file_candidate = pathlib.Path(files[0])
+        file_reference = pathlib.Path(files[1])
 
-        file_reference = pathlib.Path(ref_dir.joinpath(file_candidate.name))
-        if file_reference.is_file():
-            nccmp_out = _compare_nc_nccmp(candidate_nc=str(file_candidate),
-                                          reference_nc=str(file_reference),
-                                          stats_only=stats_only,
-                                          nccmp_options=nccmp_options,
-                                          exclude_vars=exclude_vars,
-                                          exclude_atts=exclude_atts)
-            output_list.append(nccmp_out)
-        else:
-            warnings.warn(str(file_candidate) + 'not found in ' + str(ref_dir))
+        nccmp_out = _compare_nc_nccmp(candidate_nc=str(file_candidate),
+                                      reference_nc=str(file_reference),
+                                      stats_only=stats_only,
+                                      nccmp_options=nccmp_options,
+                                      exclude_vars=exclude_vars,
+                                      exclude_atts=exclude_atts)
+        output_list.append(nccmp_out)
     return output_list
 
 
@@ -107,8 +105,14 @@ class OutputDataDiffs(object):
             reference_att = getattr(reference_output,att)
 
             if candidate_att is not None and reference_att is not None:
-                setattr(self,att,compare_ncfiles(candidate_files=candidate_att,
-                                                 reference_files=reference_att,
+                # Check that files exist in both directories
+                candidate_files = candidate_att
+                reference_files = reference_att
+
+                valid_files = _check_file_lists(candidate_files,reference_files)
+
+                setattr(self,att,compare_ncfiles(candidate_files=valid_files[0],
+                                                 reference_files=valid_files[1],
                                                  stats_only=True,
                                                  nccmp_options=nccmp_options,
                                                  exclude_vars=exclude_vars,
@@ -174,8 +178,14 @@ class OutputMetaDataDiffs(object):
             reference_att = getattr(reference_output,att)
 
             if candidate_att is not None and reference_att is not None:
-                setattr(self,att,compare_ncfiles(candidate_files=candidate_att,
-                                                 reference_files=reference_att,
+                # Check that files exist in both directories
+                candidate_files = candidate_att
+                reference_files = reference_att
+
+                valid_files = _check_file_lists(candidate_files,reference_files)
+
+                setattr(self,att,compare_ncfiles(candidate_files=valid_files[0],
+                                                 reference_files=valid_files[1],
                                                  nccmp_options=nccmp_options,
                                                  exclude_vars=exclude_vars,
                                                  exclude_atts=exclude_atts)
@@ -193,8 +203,8 @@ def _compare_nc_nccmp(candidate_nc: str,
     """Private method to compare two netcdf files using nccmp.
     This is wrapped by compare ncfiles to applying to a list of one or more files
     Args:
-        candidate_nc: The path for the candidate restart file
-        reference_nc: The path for the reference restart file
+        candidate_nc: The path for the candidate netcdf file
+        reference_nc: The path for the reference netcdf file
         stats_only: Only return statistics on differences in data values
         nccmp_options: List of long-form command line options passed to nccmp,
         see http://nccmp.sourceforge.net/ for options
@@ -257,3 +267,46 @@ def _compare_nc_nccmp(candidate_nc: str,
             return proc.stderr.decode('utf-8') + proc.stdout.decode('utf-8')
     else:
         return None
+
+def _check_file_lists(candidate_files: list,reference_files: list) -> tuple:
+    """Function to check two lists of pathlib.Paths for commonly occuring files between the two
+    Args:
+        candidate_files: The candidate file list
+        reference_files: The reference file list
+    Returns:
+        A tuple of lists sorted by file name of common files
+    """
+    candidate_names = [file.name for file in candidate_files]
+    reference_names = [file.name for file in reference_files]
+
+    # Get only files occurring in both lists
+    matching_files = list(set(candidate_names).intersection(reference_names))
+
+    # Print warning about missing files
+    missing_ref_files = [file.name for file in candidate_files if file.name not in matching_files]
+    missing_can_files = [file.name for file in reference_files if file.name not in matching_files]
+    if len(missing_ref_files) > 0:
+        if len(missing_ref_files) == 1:
+            miss_file_str = str(missing_ref_files[0])
+        else:
+            miss_file_str = ', '.join(missing_ref_files)
+        warnings.warn(
+            'The following reference files were not found in the candidate: ' + miss_file_str)
+
+    if len(missing_can_files) > 0:
+        if len(missing_can_files) == 1:
+            miss_file_str = str(missing_can_files[0])
+        else:
+            miss_file_str = ', '.join(missing_can_files)
+        warnings.warn(
+            'The following candidate files were not found in the reference: ' + miss_file_str)
+
+    # Subset lists to only those files that occur in both
+    valid_ref_files = [file for file in candidate_files if file.name in matching_files]
+    valid_can_files = [file for file in reference_files if file.name in matching_files]
+
+    # Sort files by name
+    valid_ref_files.sort(key=lambda x: x.name)
+    valid_can_files.sort(key=lambda x: x.name)
+
+    return valid_can_files, valid_ref_files

73d61eadfe1f9c4de9f59e780ccbde44c4390e37
jmills-ncar <jmills@ucar.edu>
2018-08-21 10:24:36 -0600

    ioutils tests

--- wrfhydropy/tests/test_domain.py
+++ wrfhydropy/tests/test_domain.py
@@ -1,98 +1,7 @@
-import xarray as xr
-import pytest
 import pathlib
-import netCDF4
+
 from wrfhydropy import Domain, WrfHydroStatic, WrfHydroTs
-import json
 
-
-@pytest.fixture()
-def domain_dir(tmpdir, ds_1d):
-    domain_top_dir_path = pathlib.Path(tmpdir).joinpath('example_case')
-    domain_dir_path = domain_top_dir_path.joinpath('NWM/DOMAIN')
-    restart_dir_path = domain_top_dir_path.joinpath('NWM/RESTART')
-    forcing_dir_path = domain_top_dir_path.joinpath('FORCING')
-
-    domain_top_dir_path.mkdir(parents=True)
-    domain_dir_path.mkdir(parents=True)
-    restart_dir_path.mkdir(parents=True)
-    forcing_dir_path.mkdir(parents=True)
-
-    # Make a list of DOMAIN filenames to create
-    domain_file_names = ['Fulldom_hires.nc',
-                         'Route_Link.nc',
-                         'soil_properties.nc',
-                         'GEOGRID_LDASOUT_Spatial_Metadata.nc',
-                         'geo_em.d01.nc',
-                         'spatialweights.nc',
-                         'GWBUCKPARM.nc',
-                         'hydro2dtbl.nc',
-                         'wrfinput_d01.nc',
-                         'LAKEPARM.nc',
-                         'nudgingParams.nc']
-
-    for file in domain_file_names:
-        file_path = domain_dir_path.joinpath(file)
-        ds_1d.to_netcdf(str(file_path))
-
-    # Make restart files
-    restart_file_names = ['HYDRO_RST.2011-08-26_00:00_DOMAIN1',
-                          'nudgingLastObs.2011-08-26_00:00:00.nc',
-                          'RESTART.2011082600_DOMAIN1']
-
-    for file in restart_file_names:
-        file_path = restart_dir_path.joinpath(file)
-        ds_1d.to_netcdf(str(file_path))
-
-    # Make forcing files
-    forcing_file_names = ['2011082600.LDASIN_DOMAIN1',
-                          '2011082601.LDASIN_DOMAIN1',
-                          '2011082602.LDASIN_DOMAIN1']
-
-    for file in forcing_file_names:
-        file_path = forcing_dir_path.joinpath(file)
-        ds_1d.to_netcdf(str(file_path))
-
-    # Make namelist patch files
-    hrldas_namelist = {
-        "base": {
-            "noahlsm_offline": {
-                "hrldas_setup_file": "./NWM/DOMAIN/wrfinput_d01.nc",
-                "restart_filename_requested": "./NWM/RESTART/RESTART.2011082600_DOMAIN1",
-                "indir": "./FORCING",
-            },
-            "wrf_hydro_offline": {
-                "forc_typ": 1
-            }
-        },
-        "nwm_ana": {
-            "noahlsm_offline": {},
-            "wrf_hydro_offline": {}
-        }
-    }
-
-    hydro_namelist = {
-        "base": {
-            "hydro_nlist": {
-                "geo_static_flnm": "./NWM/DOMAIN/geo_em.d01.nc",
-                "restart_file": "./NWM/RESTART/HYDRO_RST.2011-08-26_00:00_DOMAIN1",
-                "aggfactrt": 4
-            },
-            "nudging_nlist": {
-                "nudginglastobsfile": "./NWM/RESTART/nudgingLastObs.2011-08-26_00:00:00.nc"
-            }
-        },
-
-        "nwm_ana": {
-            "hydro_nlist": {},
-            "nudging_nlist": {}
-        }
-    }
-
-    json.dump(hrldas_namelist,domain_top_dir_path.joinpath('hrldas_namelist_patches.json').open('w'))
-    json.dump(hydro_namelist,domain_top_dir_path.joinpath('hydro_namelist_patches.json').open('w'))
-
-    return domain_top_dir_path
 
 def test_domain_init(domain_dir):
     domain = Domain(domain_top_dir=domain_dir,
@@ -110,7 +19,8 @@ def test_domain_namelists(domain_dir):
         'hydro_nlist':
             {'geo_static_flnm': './NWM/DOMAIN/geo_em.d01.nc',
              'restart_file': './NWM/RESTART/HYDRO_RST.2011-08-26_00:00_DOMAIN1',
-             'aggfactrt': 4},
+             'aggfactrt': 4,
+             'udmp_opt': 1},
         'nudging_nlist': {
             'nudginglastobsfile': './NWM/RESTART/nudgingLastObs.2011-08-26_00:00:00.nc'}
     }, 'hydro_namelist JSONNamelist did not return expected dictionary ' \
--- wrfhydropy/tests/test_ioutils.py
+++ wrfhydropy/tests/test_ioutils.py
@@ -1,16 +1,20 @@
+import json
+
 import pytest
-import numpy as np
-import pandas as pd
-import xarray as xr
-import pathlib
+
 from wrfhydropy.core.ioutils import *
+from wrfhydropy.core.namelist import JSONNamelist
 
 @pytest.fixture
 def ds_timeseries(tmpdir):
     ts_dir = pathlib.Path(tmpdir).joinpath('timeseries_data')
     ts_dir.mkdir(parents=True)
+
     # Create a dummy dataset
-    vals_ts = np.array([1.0,2.0,3.0],dtype='int')
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        vals_ts = np.array([np.log(-1.0),2.0,3.0],dtype='float')
+
     reference_times = pd.to_datetime(['1984-10-14 00:00:00',
                                       '1984-10-14 01:00:00',
                                       '1984-10-14 02:00:00'])
@@ -38,15 +42,13 @@ def test_open_nwmdataset_no_forecast(ds_timeseries):
                              forecast = False)
 
     assert the_ds['reference_time'].values == np.array(['1970-01-01T00:00:00.000000000'],
-                                                             dtype='datetime64[ns]')
+                                                       dtype='datetime64[ns]')
 
     the_ds['time'].values.sort()
     assert np.all(the_ds['time'].values == np.array(['1984-10-14T01:00:00.000000000',
-                                              '1984-10-14T02:00:00.000000000',
-                                              '1984-10-14T03:00:00.000000000'],
-                                                             dtype='datetime64[ns]'))
-
-    assert np.all(the_ds['var1'].values == np.array([1.0,2.0,3.0], dtype='int'))
+                                                     '1984-10-14T02:00:00.000000000',
+                                                     '1984-10-14T03:00:00.000000000'],
+                                                    dtype='datetime64[ns]'))
 
 def test_open_nwmdataset_forecast(ds_timeseries):
     ds_paths = list(ds_timeseries.rglob('*.nc'))
@@ -56,15 +58,15 @@ def test_open_nwmdataset_forecast(ds_timeseries):
 
     the_ds['reference_time'].values.sort()
     assert np.all(the_ds['reference_time'].values == np.array(['1984-10-14T00:00:00.000000000',
-                                              '1984-10-14T01:00:00.000000000',
-                                              '1984-10-14T02:00:00.000000000'],
-                                                             dtype='datetime64[ns]'))
+                                                               '1984-10-14T01:00:00.000000000',
+                                                               '1984-10-14T02:00:00.000000000'],
+                                                              dtype='datetime64[ns]'))
 
     the_ds['time'].values.sort()
     assert np.all(the_ds['time'].values == np.array(['1984-10-14T01:00:00.000000000',
-                                              '1984-10-14T02:00:00.000000000',
-                                              '1984-10-14T03:00:00.000000000'],
-                                                             dtype='datetime64[ns]'))
+                                                     '1984-10-14T02:00:00.000000000',
+                                                     '1984-10-14T03:00:00.000000000'],
+                                                    dtype='datetime64[ns]'))
     # print(the_ds)
     # print(the_ds['var1'].values)
     # assert np.all(the_ds['var1'].values == np.array([[[1.0,2.0,3.0]]], dtype='int'))
@@ -75,18 +77,35 @@ def test_wrfhydrots(ds_timeseries):
     ts_obj_open = ts_obj.open()
 
     assert type(ts_obj_open) == xr.core.dataset.Dataset
-
-    # TODO add test for checkNAs
-    #assert ts_obj.check_nas == None
+    assert type(ts_obj.check_nas()) == pd.DataFrame
 
 def test_wrfhydrostatic(ds_timeseries):
 
-    st_obj = WrfHydroStatic(list(ds_timeseries.rglob('*.nc'))[0])
+    static_obj = WrfHydroStatic(list(ds_timeseries.rglob('*.nc'))[0])
 
-    st_obj_open = st_obj.open()
+    static_obj_open = static_obj.open()
 
-    assert type(st_obj_open) == xr.core.dataset.Dataset
+    assert type(static_obj_open) == xr.core.dataset.Dataset
+    assert type(static_obj.check_nas()) == pd.DataFrame
 
-    # TODO add test for checkNAs
-    #assert ts_obj.check_nas == None
+def test_check_input_files(domain_dir):
+    hrldas_namelist = JSONNamelist(domain_dir.joinpath('hrldas_namelist_patches.json'))
+    hrldas_namelist = hrldas_namelist.get_config('nwm_ana')
+    hydro_namelist = JSONNamelist(domain_dir.joinpath('hydro_namelist_patches.json'))
+    hydro_namelist = hydro_namelist.get_config('nwm_ana')
+
+    input_file_check = check_input_files(hrldas_namelist=hrldas_namelist,
+                                         hydro_namelist=hydro_namelist,
+                                         sim_dir=domain_dir)
+    assert input_file_check is None
+
+    # Alter one file to cause a false in check_input_files
+    hydro_namelist['hydro_nlist']['geo_static_flnm'] = 'no_such_file'
+
+    with pytest.raises(ValueError) as excinfo:
+        check_input_files(hrldas_namelist=hrldas_namelist,
+                          hydro_namelist=hydro_namelist,
+                          sim_dir=domain_dir)
+
+    assert str(excinfo.value) == 'The namelist file geo_static_flnm = no_such_file does not exist'
 

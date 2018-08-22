0bd0f841ae4c392d83b6b3cd89565b9f038a4584
jmills-ncar <jmills@ucar.edu>
2018-08-22 16:13:33 -0600

    output diffs tests

--- wrfhydropy/tests/test_outputdiffs.py
+++ wrfhydropy/tests/test_outputdiffs.py
@@ -15,21 +15,19 @@ def test_outputdiffs_outputdatadiffs(sim_output):
 
     output=SimulationOutput()
     output.collect_output(sim_dir=sim_output)
-    public_atts = [att for att in dir(output) if not att.startswith('__')]
-    for att in public_atts:
-        assert getattr(output,att) is not None
 
-    assert output.check_output_nas() is not None
+    output_diffs = OutputDataDiffs(output,output)
+
+    assert output_diffs.diff_counts == {'channel_rt': 0, 'chanobs': 0, 'lakeout': 0, 'gwout': 0,
+                                     'restart_hydro': 0, 'restart_lsm': 0, 'restart_nudging': 0}
 
 
-    #
-    # output = SimulationOutput()
-    # output.collect_output(sim_output)
-    #
-    # output_diffs = OutputDataDiffs(output,output)
-    #
-    # print(output.channel_rt)
-    # print(output_diffs.diff_counts)
-    # assert 1==2
-    #
+def test_outputdiffs_outputmetadatadiffs(sim_output):
 
+    output=SimulationOutput()
+    output.collect_output(sim_dir=sim_output)
+
+    output_diffs = OutputMetaDataDiffs(output,output)
+
+    assert output_diffs.diff_counts == {'channel_rt': 0, 'chanobs': 0, 'lakeout': 0, 'gwout': 3,
+                                     'restart_hydro': 0, 'restart_lsm': 0, 'restart_nudging': 0}

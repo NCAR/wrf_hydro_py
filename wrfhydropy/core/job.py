f05aae1926b79ede4e755d8a8ae9c680e3b95f4f
jmills-ncar <jmills@ucar.edu>
2018-09-13 07:56:52 -0600

    only one of kday or khour written

--- wrfhydropy/core/job.py
+++ wrfhydropy/core/job.py
@@ -246,9 +246,11 @@ class Job(object):
             duration = self._model_end_time - self._model_start_time
             if duration.seconds == 0:
                 self._hrldas_times['noahlsm_offline']['kday'] = int(duration.days)
+                del self._hrldas_times['noahlsm_offline']['khour']
             else:
                 self._hrldas_times['noahlsm_offline']['khour'] =int(duration.days * 60 +
                                                                     duration.seconds / 3600)
+                del self._hrldas_times['noahlsm_offline']['kday']
 
             # Start
             self._hrldas_times['noahlsm_offline']['start_year'] = int(self._model_start_time.year)
@@ -392,7 +394,9 @@ class Job(object):
             warnings.warn('model start or end time was not specified in job, start end times will \
             be used from supplied namelist')
             self._model_start_time, self._model_end_time = self._solve_model_start_end_times()
-        return self._hrldas_namelist.patch(self.hrldas_times)
+        hrldas_nl = self._hrldas_namelist.patch(self.hrldas_times)
+
+        return
 
     @property
     def model_start_time(self):

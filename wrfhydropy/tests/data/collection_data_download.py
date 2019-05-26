9cf52088cc1ea446d7580cbb2c09320e6d928fc7
James McCreight <jamesmcc@ucar.edu>
2019-05-25 21:39:05 -0700

    Fix tests/data problem

--- wrfhydropy/tests/data/collection_data_download.py
+++ wrfhydropy/tests/data/collection_data_download.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 import shutil
 from .gdrive_download import download_file_from_google_drive, untar
@@ -5,6 +6,9 @@ from wrfhydropy.core.ioutils import md5
 
 
 def download(version='latest'):
+    orig_dir = pathlib.Path('.').resolve()
+    data_dir = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(data_dir)
     id_md5_dict = {
         'latest': {
             'id': '1VrWVve8fhYobDg2xDrgHfiAi7VBDmV9T',
@@ -17,6 +21,7 @@ def download(version='latest'):
     if target_name.exists() and md5(target_name) == the_md5:
         if not pathlib.Path('collection_data').exists():
             untar(str(target_name))
+        os.chdir(orig_dir)
         return None
     if target_name.exists():
         target_name.unlink()
@@ -24,6 +29,7 @@ def download(version='latest'):
         shutil.rmtree('collection_data')
     download_file_from_google_drive(id, str(target_name))
     untar(str(target_name))
+    os.chdir(orig_dir)
 
 
 if __name__ == "__main__":

e5ea70a6af6d5f1376dce10e27c869a72eb21426
Ishita Srivastava <ishitas@cheyenne1.ib0.cheyenne.ucar.edu>
2020-06-30 21:19:44 -0600

    added changes to open_wh_dataset function

--- wrfhydropy/core/ioutils.py
+++ wrfhydropy/core/ioutils.py
@@ -349,8 +349,10 @@ def open_wh_dataset(paths: list,
         ds = xr.open_dataset(a_file, chunks=chunks, mask_and_scale=False)
         # Check if forecast and set reference_time to zero if not
         if not forecast:
-            ds.coords['reference_time'].values = np.array(
-                [np.datetime64('1970-01-01T00:00:00', 'ns')])
+            ds.assign_coords({"reference_time": np.array(
+                [np.datetime64('1970-01-01T00:00:00', 'ns')])})
+            #ds.coords['reference_time'].values = np.array(
+            #    [np.datetime64('1970-01-01T00:00:00', 'ns')])
 
         ref_time = ds['reference_time'].values[0]
         if ref_time in ds_dict:

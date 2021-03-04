24053de280b43e94932f143e2071bc0b68c5f09c
James McCreight <jamesmcc@ucar.edu>
2021-03-03 22:37:27 -0700

    dressup the setup too

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='wrfhydropy',
     version='0.0.18',
@@ -28,5 +31,8 @@ setup(
     ],
     author='James McCreight',
     author_email='jamesmcc@ucar.edu',
-    description='Crude API for the WRF-Hydro model',
+    description='API for the WRF-Hydro model',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
 )
